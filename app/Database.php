a39991c7e97e12627e1732bb6f5ce977c4f968c6
Patrick Laubscher <patricklaubscher@hotmail.com>
2023-12-21 22:51:36 +0100

    db

---
+++ app/Database.php
@@ -0,0 +1,41 @@
+<?php 
+
+namespace App;
+
+use \PDO; 
+
+class Database {
+
+    private $db_name;
+    private $db_user;
+    private $db_pass;
+    private $db_host;
+    private $pdo;
+
+
+    public function __construct($db_name, $db_user = 'patrick', $db_pass = 'root', $db_host = 'localhost')
+    {
+        $this->db_name = $db_name;
+        $this->db_user = $db_user;
+        $this->db_pass = $db_pass;
+        $this->db_host = $db_host;
+
+    }
+
+    private function getPDO(){
+        if($this->pdo === null){
+            $pdo = new PDO('mysql:dbname=blog;host=localhost', 'patrick', 'root');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+        }
+        return $this->pdo;
+    }
+
+    public function query($statement){
+        $req = $this->getPDO()->query($statement);      
+        $data = $req->fetchAll(PDO::FETCH_OBJ);
+        return $data; 
+    }
+
+
+}
